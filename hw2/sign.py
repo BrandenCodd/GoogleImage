7ce47893ac8cf05357ab63a7c38fcf3d70ece55f
Branden Codd <coddb@sou.edu>
2021-01-30 17:00:06 -0800

    initial implmentation of sign.py

---
+++ hw2/sign.py
@@ -0,0 +1,17 @@
+from flask import redirect, request, url_for, render_template
+from flask.views import MethodView
+import gbmodel
+
+class Sign(MethodView):
+    def get(self):
+        return render_template('sign.html')
+
+    def post(self):
+        """
+        Accepts POST requests, and processes the form;
+        Redirect to index when completed.
+        """
+        model = gbmodel.get_model()
+        model.insert(request.form['name'], request.form['description'], request.form['streetAdress'],request.form['typesOfService'], \ 
+        request.form['phoneNumber'],request.form['hoursOfOperation'],request.form['reviews'])
+        return redirect(url_for('index'))
