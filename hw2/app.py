d2eb49dbc31317bbf2f873995c138ca6b5bb5180
Branden Codd <coddb@sou.edu>
2021-01-30 19:48:41 -0800

    updated the nameing scheme from sign to info for a few files

--- hw2/app.py
+++ hw2/app.py
@@ -4,7 +4,7 @@ A flask app for listing charities and social services in portland.
 import flask
 from flask.views import MethodView
 from index import Index
-from sign import Sign
+from info import Info
 
 app = flask.Flask(__name__)       # our Flask app
 
@@ -12,8 +12,8 @@ app.add_url_rule('/',
                  view_func=Index.as_view('index'),
                  methods=["GET"])
 
-app.add_url_rule('/sign/',
-                 view_func=Sign.as_view('sign'),
+app.add_url_rule('/info/',
+                 view_func=Info.as_view('info'),
                  methods=['GET', 'POST'])
 
 if __name__ == '__main__':
