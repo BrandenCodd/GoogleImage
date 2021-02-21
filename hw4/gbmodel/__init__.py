79cae7b7c149d129155b4329dff3338c5545e034
Branden Codd <coddb@sou.edu>
2021-02-20 20:21:20 -0800

    updated init to use datastore

--- hw4/gbmodel/__init__.py
+++ hw4/gbmodel/__init__.py
@@ -5,6 +5,8 @@ if model_backend == 'sqlite3':
     from .model_sqlite3 import model
 elif model_backend == 'pylist':
     from .model_pylist import model
+elif model_backend == 'dynamodb':
+    from .model_dynamodb import model
 else:
     raise ValueError("No appropriate databackend configured. ")
 
