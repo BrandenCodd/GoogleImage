5636d4c1c0ed2fc91a49a7adbd0379e182f993c6
Branden Codd <coddb@sou.edu>
2021-03-09 12:42:45 -0800

    deleteing hw4 code and updates, starting over

--- final/index.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from flask.views import MethodView
-import gbmodel
-
-class Index(MethodView):
-    def get(self):
-        model = gbmodel.get_model()
-        entries = [dict(description=row[0] ) for row in model.select()]
-        return render_template('index.html',entries=entries)
