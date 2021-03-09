be75442b5289702a1c27ab1bdd275e1e2be6be98
Branden Codd <coddb@sou.edu>
2021-03-09 12:38:39 -0800

    updating index.py parameters

--- final/index.py
+++ final/index.py
@@ -5,5 +5,5 @@ import gbmodel
 class Index(MethodView):
     def get(self):
         model = gbmodel.get_model()
-        entries = [dict(name=row[0], description=row[1], streetAddress=row[2], typesOfService=row[3], phoneNumber=row[4], hoursOfOperation=row[5], reviews=row[6] ) for row in model.select()]
+        entries = [dict(description=row[0] ) for row in model.select()]
         return render_template('index.html',entries=entries)
