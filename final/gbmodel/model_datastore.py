c63ef53c5df111e43a2e91870295490701744345
Branden Codd <coddb@sou.edu>
2021-03-09 12:32:49 -0800

    added hw4 to start as a base for the final

---
+++ final/gbmodel/model_datastore.py
@@ -0,0 +1,60 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .Model import Model
+from datetime import datetime
+from google.cloud import datastore
+
+def from_datastore(entity):
+    """Translates Datastore results into the format expected by the
+    application.
+
+    Datastore typically returns:
+        [Entity{key: (kind, id), prop: val, ...}]
+
+    This returns:
+        [ name, description, streetAddress, typesOfService, phoneNumber, hoursOfOperation, review ]
+    where name, description, streetAddress, typesOfService, phoneNumber, hoursOfOperation, review are Python strings
+    """
+    if not entity:
+        return None
+    if isinstance(entity, list):
+        entity = entity.pop()
+    return [entity['name'],entity['description'],entity['streetAddress'],entity['typesOfService'], entity['phoneNumber'], entity['hoursOfOperation'],entity['reviews']]
+
+class model(Model):
+    def __init__(self):
+        self.client = datastore.Client('cs356-w21-branden-codd')
+
+    def select(self):
+        query = self.client.query(kind = 'hw4')
+        entities = list(map(from_datastore,query.fetch()))
+        entities = [x for x in entities if x != None]
+        return entities
+
+    def insert(self,name,description,streetAddress,typesOfService,phoneNumber,
+hoursOfOperation,reviews):
+        key = self.client.key('hw4')
+        rev = datastore.Entity(key)
+        rev.update( {
+            'name': name,
+            'description': description,
+            'streetAddress' : streetAddress,
+            'typesOfService' : typesOfService,
+            'phoneNumber' : phoneNumber,
+            'hoursOfOperation' : hoursOfOperation,
+            'reviews' : reviews
+            })
+        self.client.put(rev)
+        return True
